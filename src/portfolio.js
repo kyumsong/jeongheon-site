7a7212c56251f74c708c1c91aab59cef60f5f27f
David Song <davidkyum.song@gmail.com>
2025-08-20 17:25:01 -0700

    Add description and change email

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,8 +8,24 @@ const about = {
   // all the properties are optional - can be left empty or deleted
   name: '안녕하세요 송정헌 목사 입니다',
 
-  description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  description: `
+저는 개혁주의 교단인 합동교단에 속한 목사입니다.
+
+교리적으로, 
+개혁주의가 성경의 진리를 가장 잘 표현하는 교리라고 믿습니다. 
+그에 따라 웨스트민스터 신앙고백과 대소요리 문답을 교리적 표준문서로 존중합니다. 
+
+목회적으로,
+교회가 세상으로부터 부름받은 하나님의 백성들이자. 세상에 하나님나라 복음을 전하기 위한 대행기관으로 믿습니다. 
+그에 따라 하나님의 백성들을 제자로 훈련하여 세상에 파송한다는 제자훈련 철학을 가지고 있습니다.
+
+설교자로서,
+성경이 성도를 교훈하고 책망하며 바르게 교정함으로 하나님이 원하시는 의의 기준에 따라 살도록 만든다고 믿습니다.
+그에 따라 성경 안에 담긴 하나님의 뜻을 순전하게 풀어 전하는 설교를 하고 있습니다.
+
+저는 58세입니다. 아직은 목회자로 더 섬길 수 있는 나이이지만, 일반적인 청빙자격이 만 55세이므로 청빙지원을 할 수 있는 나이는 지났습니다. 그래서 하나님의 
+은혜를 기다리고 있습니다. 좀 더 섬길 수 있는 기회가 있다면 좋겠습니다.
+  `,
 }
 
 const projects = [
@@ -31,7 +47,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'jeongheon.song@mail.com',
+  email: 'sjhdavid@naver.com',
 }
 
 export { header, about, projects, skills, contact }
